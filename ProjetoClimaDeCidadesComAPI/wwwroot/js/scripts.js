c2631dee45bc6f6effd3469cfb0e7a129bfee7c4
Relinton Pinheiro <relinton.pinheiro.infouniron@gmail.com>
2022-10-14 12:49:55 -0400

    Projeto de Busca Clima por Cidade via API em Asp Net Core

---
+++ ProjetoClimaDeCidadesComAPI/wwwroot/js/scripts.js
@@ -0,0 +1,56 @@
+﻿//Variáveis e seleção de elementos
+const apiKey = "Sua chave aqui rs";
+const apiPaisUrl = "https://countryflagsapi.com/png/";
+
+const cidadeInput = document.querySelector("#input-cidade");
+const searchBtn = document.querySelector("#search");
+
+const cidadeElemento = document.querySelector("#cidade");
+const temperaturaEmento = document.querySelector("#temperatura span");
+const descricaoElemento = document.querySelector("#descricao");
+const iconeMetereologicoElemento = document.querySelector("#icon-metereologico");
+const paisElemento = document.querySelector("#pais");
+const umidadeElemento = document.querySelector("#umidade span");
+const ventoElemento = document.querySelector("#vento span");
+
+const containerMetereologico = document.querySelector("#dados-metereologicos");
+
+//Funções
+const buscarDadosMetereologicos = async (cidade) => {
+    const apiMetereologicaURL = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+    const res = await fetch(apiMetereologicaURL)
+    const dados = await res.json()
+    return dados;
+};
+
+const mostrarDadosMetereologicos = async (cidade) => {
+    const dados = await buscarDadosMetereologicos(cidade);
+
+    cidadeElemento.innerText = dados.name;
+    temperaturaEmento.innerText = parseInt(dados.main.temp);
+    descricaoElemento.innerText = dados.weather[0].description;
+    iconeMetereologicoElemento.setAttribute("src", `http://openweathermap.org/img/wn/${dados.weather[0].icon}.png`);
+    paisElemento.setAttribute("src", apiPaisUrl + dados.sys.country);
+    umidadeElemento.innerText = `${dados.main.humidity}%`;
+    ventoElemento.innerText = `${dados.wind.speed}km/h`;
+
+    containerMetereologico.classList.remove("hide");
+};
+
+
+//Eventos
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const cidade = cidadeInput.value;
+
+    mostrarDadosMetereologicos(cidade);
+});
+
+cidadeInput.addEventListener("keyup", (e) => {
+    if (e.code === "Enter") {
+        const cidade = e.target.value;
+        buscarDadosMetereologicos(cidade);
+    }
+})
